db894bbedaffc1f676e6bee74c3662e154b32488
Ana Maria Oliveira Moraes <147004089+AnaDw1@users.noreply.github.com>
2023-11-24 15:45:14 -0300

    Update schema.sql
    só mudei que tlg, fique mais lógico na leitura do código, então tipo, conversa - mensagem - tipo conteudo, e mudei pra que mensagem se conecte apenas com conversa e não com pessoa, porém não se tá certo isso.

--- schema.sql
+++ schema.sql
@@ -2,78 +2,85 @@ CREATE DATABASE DireitosConnect;
 
 USE DireitosConnect;
 
-CREATE TABLE pessoas (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
+CREATE TABLE pessoa (
+    id INT PRIMARY KEY IDENTITY,
     CPF CHAR(11) NOT NULL UNIQUE,
-    nome VARCHAR(50),
-    email VARCHAR(50) NOT NULL UNIQUE,
-    senha VARCHAR(50) NOT NULL
+    nome VARCHAR(100),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL 
 );
 
-CREATE TABLE clientes (
-    id_pessoa INT NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id_pessoa) REFERENCES pessoas (id)
+CREATE TABLE cliente (
+    id_pessoa INT PRIMARY KEY,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
 );
 
-CREATE TABLE advogados (
-    id_pessoa INT NOT NULL PRIMARY KEY,
-    FOREIGN KEY (id_pessoa) REFERENCES pessoas (id),
-    cadastro_oab VARCHAR(20) NOT NULL UNIQUE
+CREATE TABLE advogado (
+    id_pessoa INT PRIMARY KEY,
+    cadastro_oab VARCHAR(20) NOT NULL UNIQUE,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
 );
 
-CREATE TABLE conversas (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
+CREATE TABLE conversa (
+    id INT PRIMARY KEY IDENTITY,
     id_remetente INT NOT NULL,
     id_destinatario INT NOT NULL,
-    FOREIGN KEY (id_remetente) REFERENCES pessoas (id),
-    FOREIGN KEY (id_destinatario) REFERENCES pessoas (id)
+    FOREIGN KEY (id_remetente) REFERENCES pessoa (id),
+    FOREIGN KEY (id_destinatario) REFERENCES pessoa (id)
 );
 
-CREATE TABLE mensagens (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
+
+CREATE TABLE mensagem (
+    id INT PRIMARY KEY IDENTITY,
     id_conversa INT NOT NULL,
-    FOREIGN KEY (id_conversa) REFERENCES conversas (id),
     id_emissor INT NOT NULL,
-    FOREIGN KEY (id_emissor) REFERENCES pessoas (id),
-    tipo_conteudo VARCHAR(20), -- 'imagem' / 'video' / 'texto'
-    conteudo VARCHAR(255)
+    id_tipo_conteudo INT NOT NULL,
+    conteudo VARCHAR(255),
+    FOREIGN KEY (id_conversa) REFERENCES conversa (id),
 );
 
-CREATE TABLE areas_juridicas (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
-    nome VARCHAR(50)
+CREATE TABLE tipo_conteudo (
+    id INT PRIMARY KEY IDENTITY,
+    nome VARCHAR(20) NOT NULL,
+	id_mensagem int not null,
+	foreign key(id_mensagem) references mensagem (id)
 );
 
-CREATE TABLE tipo_assinaturas (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
-    nome VARCHAR(30) NOT NULL,
-    valor FLOAT NOT NULL
-);
-
-CREATE TABLE assinaturas (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
-    id_tipo_assinatura INT NOT NULL,
-    FOREIGN KEY (id_tipo_assinatura) REFERENCES tipo_assinaturas (id),
-    id_pessoa INT NOT NULL,
-    FOREIGN KEY (id_pessoa) REFERENCES pessoas (id)
-);
-
-CREATE TABLE casos_juridicos (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
-    titulo VARCHAR(50),
+CREATE TABLE caso_juridico (
+    id INT PRIMARY KEY IDENTITY,
+    titulo VARCHAR(100),
     descricao VARCHAR(255),
     id_cliente INT NOT NULL,
     id_advogado INT NOT NULL,
-    id_area_juridica INT NOT NULL,
-    FOREIGN KEY (id_area_juridica) REFERENCES areas_juridicas (id),
-    FOREIGN KEY (id_cliente) REFERENCES pessoas (id),
-    FOREIGN KEY (id_advogado) REFERENCES pessoas (id)
+    FOREIGN KEY (id_cliente) REFERENCES cliente (id_pessoa),
+    FOREIGN KEY (id_advogado) REFERENCES advogado (id_pessoa),
 );
 
-CREATE TABLE pagamentos (
-    id INT NOT NULL PRIMARY KEY IDENTITY,
-    forma_pagamento VARCHAR(30),
+CREATE TABLE area_juridica (
+    id INT PRIMARY KEY IDENTITY,
+    nome VARCHAR(100),
+	id_area_juridica int not null,
+ FOREIGN KEY (id_area_juridica) REFERENCES caso_juridico (id)
+);
+
+CREATE TABLE assinatura (
+    id INT PRIMARY KEY IDENTITY,
+    id_pessoa INT NOT NULL,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
+);
+
+CREATE TABLE tipo_assinatura (
+    id INT PRIMARY KEY IDENTITY,
+    nome VARCHAR(30) NOT NULL,
+	id_tipo_assinatura INT NOT NULL,
+    valor FLOAT NOT NULL,
+	FOREIGN KEY (id_tipo_assinatura) REFERENCES assinatura (id),
+);
+
+CREATE TABLE pagamento (
+    id INT PRIMARY KEY IDENTITY,
+    forma_pagamento VARCHAR(50),
     data_pagamento DATE,
     id_assinatura INT NOT NULL,
-    FOREIGN KEY (id_assinatura) REFERENCES assinaturas (id)
+    FOREIGN KEY (id_assinatura) REFERENCES tipo_assinatura (id)
 );
